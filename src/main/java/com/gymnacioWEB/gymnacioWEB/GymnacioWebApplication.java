310ae6e393236cf59d933af7c629275cbe8c6c15
Naelerd <juampi_22_55@hotmail.com>
2024-01-15 16:15:11 -0300

    Commit 1.0

---
+++ src/main/java/com/gymnacioWEB/gymnacioWEB/GymnacioWebApplication.java
@@ -0,0 +1,14 @@
+package com.gymnacioWEB.gymnacioWEB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymnacioWebApplication {
+    
+        
+	public static void main(String[] args) {
+		SpringApplication.run(GymnacioWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gymnacioWEB/gymnacioWEB/GymnacioWebApplicationTests.java
@@ -0,0 +1,15 @@
+package com.gymnacioWEB.gymnacioWEB;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class GymnacioWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+        
+}
