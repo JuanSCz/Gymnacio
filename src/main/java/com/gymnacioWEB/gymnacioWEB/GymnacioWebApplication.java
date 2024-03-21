6cba951fc6d0cb87965dda75c53bc8ec32f6cb3c
Naelerd <juampi_22_55@hotmail.com>
2024-03-21 00:56:01 -0300

    Commit 4.0

--- src/main/java/com/gymnacioWEB/gymnacioWEB/GymnacioWebApplication.java
+++ src/main/java/com/gymnacioWEB/gymnacioWEB/GymnacioWebApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GymnacioWebApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(GymnacioWebApplication.class, args);
     }
